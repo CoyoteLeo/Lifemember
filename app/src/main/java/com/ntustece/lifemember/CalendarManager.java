60a226cf9ed85bba849fdc37d68fcc6ba05fd269
Petingo <eyanghongbin0315@gmail.com>
2017-08-14 14:19:24 +0800

    Initial commit

---
+++ app/src/main/java/com/ntustece/lifemember/CalendarManager.java
@@ -0,0 +1,9 @@
+package com.ntustece.lifemember;
+
+/**
+ * Created by petingo on 2017/8/14.
+ */
+
+public class CalendarManager {
+
+}
---
+++ app/src/main/java/com/ntustece/lifemember/Question.java
@@ -0,0 +1,16 @@
+package com.ntustece.lifemember;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by petingo on 2017/8/14.
+ */
+
+public class Question extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.question);
+    }
+}
